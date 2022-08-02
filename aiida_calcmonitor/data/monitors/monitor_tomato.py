4fb8309b498f754e3d29cc899bf9a0b6872de71b
krpe <peter.kraus@empa.ch>
2022-08-02 14:10:11 +0200

    Add monitor script for debug

---
+++ aiida_calcmonitor/data/monitors/monitor_tomato.py
@@ -0,0 +1,29 @@
+"""
+Monitor example for the toy model.
+"""
+import os
+import json
+from aiida_calcmonitor.data.monitors.monitor_base import MonitorBase
+
+class MonitorTomatoDummy(MonitorBase):  # pylint: disable=too-many-ancestors
+    """Example of monitor for a tomato's dummy job."""
+
+    def monitor_analysis(self):
+        sources = self['sources']
+        options = self['options']
+
+        filepath = sources['output']['filepath']
+
+        with open(filepath, "rb") as fileobj:
+            jsdata = json.load(fileobj)
+        
+        last_ts = jsdata["steps"][0]["data"][-1]
+        last_val = last_ts["raw"]["value"]
+        print(f'value under control: {last_val}')
+
+        max_val = options.get("maximum_value", 10)
+        if last_val < max_val:
+            return None
+        else:
+            print('value exceeded!')
+            return 'value exceeded!'
